b37da4773c9c911fa4d0b48d7be980fd04b90108
Rene Laas <rene_laas@hotmail.com>
2025-01-03 21:27:39 +0100

    updat gems

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,7 @@
-var store = []
+var store = [{
+        "title": "Configure Defender SmartScreen via Settings catalog with Intune",
+        "excerpt":"The purpose of this blog post is to inform you how to configure Microsoft Defender SmartScreen in Windows and Microsoft Edge via the Settings catalog option with Microsoft Intune. In this blog post, I use the Settings catalog option, it is possible to configure SmartScreen via several methods. For example...","categories": ["Microsoft-Defender-For-XDR","SmartScreen"],
+        "tags": ["Intune","SmartScreen","Microsoft-Defender-For-XDR","Settings-Catalog"],
+        "url": "/microsoft-defender-for-xdr/smartscreen/Configure-Defender-SmartScreen-via-Settings-catalog-with-Intune.md/",
+        "teaser": null
+      }]
